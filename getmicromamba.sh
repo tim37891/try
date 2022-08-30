67d3f56d12616b47062379c71df739c062cc2c90
Tc <timgc.37891@gmail.com>
2022-08-30 02:40:44 +0000

    test

--- getmicromamba.sh
+++ getmicromamba.sh
@@ -3,4 +3,5 @@ curl micro.mamba.pm/install.sh | bash
 echo "micromamba activate" >> ~/.bashrc
 # Restart
 exec bash
-micromamba install -c conda-forge gh git gnupg
+micromamba install -c conda-forge mamba
+mamba install --yes --quiet -c conda-forge gh git gnupg
