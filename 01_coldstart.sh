fedd53d27e7f4ef121c8cea442c6e61fc5489ad6
timgc <timgc.37891@gmail.com>
2025-11-18 02:40:21 +0000

    save

--- 01_coldstart.sh
+++ 01_coldstart.sh
@@ -9,6 +9,6 @@
 NEW_USER_NAME=$1
 sudo adduser --disabled-password --gecos "" ${NEW_USER_NAME}
 sudo usermod -aG sudo ${NEW_USER_NAME}
-sudo echo "$NEW_USER_NAME  ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/init_${NEW_USER_NAME}
+echo "$NEW_USER_NAME  ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/init_${NEW_USER_NAME} > /dev/null
 sudo su --login --pty ${NEW_USER_NAME}
 #####################################################################################
