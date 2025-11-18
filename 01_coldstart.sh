5909fe4ee5638d7150db2bde93db32c04f69681e
timgc <timgc.37891@gmail.com>
2025-11-18 02:22:03 +0000

    save

--- 01_coldstart.sh
+++ 01_coldstart.sh
@@ -9,5 +9,6 @@
 NEW_USER_NAME=$1
 sudo adduser --disabled-password --gecos "" ${NEW_USER_NAME}
 sudo usermod -aG sudo ${NEW_USER_NAME}
+echo "$NEW_USER_NAME  ALL=(ALL:ALL) ALL" >> /etc/sudoers
 sudo su --login --pty ${NEW_USER_NAME}
 #####################################################################################
