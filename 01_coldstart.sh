6e2f2f54d5733035661a3d64555dc30f7745802e
timgc <timgc.37891@gmail.com>
2025-11-18 01:58:28 +0000

    save

---
+++ 01_coldstart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#####################################################################################
+# cd ~
+# git clone https://github.com/tim37891/try.git
+# new user setup
+NEW_USER_NAME=$1
+sudo adduser --disabled-password --gecos "" ${NEW_USER_NAME}
+sudo usermod -aG sudo ${NEW_USER_NAME}
+sudo su --login --pty ${NEW_USER_NAME}
+#####################################################################################
--- 02_coldstart.sh
+++ 02_coldstart.sh
@@ -5,14 +5,6 @@ sudo apt update -y
 sudo apt dist-upgrade -y
 sudo apt install -y git gnupg pinentry-tty openssh-client openssh-server
 #####################################################################################
-# new user setup
-NEW_USER_NAME=$1
-sudo adduser --disabled-password --gecos "" ${NEW_USER_NAME}
-sudo usermod -aG sudo ${NEW_USER_NAME}
-sudo su --login --pty ${NEW_USER_NAME}
-cd ~
-git clone https://github.com/tim37891/try.git
-#####################################################################################
 # gpg setup
 mkdir -p ~/.gnupg
 echo "pinentry-program /usr/bin/pinentry-tty" >> ~/.gnupg/gpg-agent.conf
