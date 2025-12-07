f4c53309ffbc55db68ff751d84050aa20793d12d
timgc <timgc.37891@gmail.com>
2025-12-07 02:16:26 +0000

    save

--- 02_coldstart.sh
+++ 02_coldstart.sh
@@ -8,7 +8,7 @@
 # system update
 sudo apt update -y
 sudo apt dist-upgrade -y
-sudo apt install -y git gnupg pinentry-tty
+sudo apt install -y git gnupg pinentry-tty openssh-client openssh-server curl
 #####################################################################################
 # gpg setup
 mkdir -p ~/.gnupg
@@ -19,8 +19,13 @@ chmod 700 ~/.gnupg
 gpg-connect-agent reloadagent /bye
 #####################################################################################
 # git setup
-eval "$(ssh-agent -s)"
-gpg --no-symkey -d $1 | ssh-add -
+gpg --no-symkey -d $1 >>  ~/.ssh/unlock
+chmod 600 ~/.ssh/unlock
+cat << 'EOF' >> ~/.ssh/config
+Host github.com
+  User git
+  IdentityFile ~/.ssh/unlock
+EOF
 ssh -y -T git@github.com
 mkdir -p ~/src
 cd ~/src
