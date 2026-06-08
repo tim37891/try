e2b316c86a1bfd275e091e587b6958b8b9d1ceb9
step <step>
2026-06-08 08:18:50 -0400

    test

--- 02_coldstart.sh
+++ 02_coldstart.sh
@@ -39,8 +39,9 @@ EOF
 ################################################
 # Pixi
 ################################################
-curl -fsSL https://pixi.sh/install.sh | zsh
+curl -fsSL https://pixi.sh/install.sh | sh
 export PATH="${HOME}/.pixi/bin:${PATH}"
+echo 'export PATH="${HOME}/.pixi/bin:${PATH}"' >> ~/.zshrc
 pixi config set shell.change-ps1 false --global
 # direnv
 pixi global install direnv
